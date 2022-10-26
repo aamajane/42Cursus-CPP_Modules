85c50719cc4a55bd310cb8095c6f14a6472f0c17
aamajane <aamajane@student.1337.ma>
2022-10-26 22:13:37 +0100

    finish cpp module 00 ex00

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -5,8 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/25 22:35:48 by aamajane          #+#    #+#             */
-/*   Updated: 2022/10/25 22:35:49 by aamajane         ###   ########.fr       */
+/*   Created: 2022/10/26 22:06:28 by aamajane          #+#    #+#             */
+/*   Updated: 2022/10/26 22:12:19 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < ac; i++)
+			for (int j = 0; av[i][j]; j++)
+				std::cout << (char)toupper(av[i][j]);
+	}
+	std::cout << std::endl;
+	return (EXIT_SUCCESS);
+}
