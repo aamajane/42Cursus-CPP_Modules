c82f825c5c8d70316ff486344ce593fb55243c65
aamajane <aamajane@student.1337.ma>
2022-11-03 23:52:48 +0100

    add a while loop to check if index is valid in SEARCH command

--- cpp_module_00/ex01/PhoneBook.cpp
+++ cpp_module_00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 21:19:52 by aamajane          #+#    #+#             */
-/*   Updated: 2022/11/03 23:22:51 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/11/03 23:50:31 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,26 +39,36 @@ void	PhoneBook::searchContact(void)
 	{
 		std::cout << "PhoneBook is empty" << std::endl;
 		return;
-	}
+	}	
 	std::cout << "     Index|First Name| Last Name|  Nickname" << std::endl;
 	for (int i = 0; i < _contactCount; i++)
 	{
 		std::cout << std::setw(10) << i << "|";
 		_contacts[i].printContact();
 	}
-	std::cout << "Enter the index of the contact you want to see: ";
-	std::string input;
-	std::getline(std::cin, input);
-	if (std::cin.eof())
-		exit(0);
-	if (input.length() == 1 && input[0] >= '0' && input[0] <= '7')
+	bool validIndex = false;
+	while (!validIndex)
 	{
-		int index = input[0] - '0';
-		if (index < _contactCount)
-			_contacts[index].printAll();
+		std::string input;
+		while (input.empty())
+		{
+			std::cout << "Enter the index of the contact you want to see: ";
+			std::getline(std::cin, input);
+			if (std::cin.eof())
+				exit(0);
+		}
+		if (input.length() == 1 && input[0] >= '0' && input[0] <= '7')
+		{
+			int index = input[0] - '0';
+			if (index < _contactCount)
+			{
+				validIndex = true;
+				_contacts[index].printAll();
+			}
+			else
+				std::cout << "Invalid index" << std::endl;
+		}
 		else
 			std::cout << "Invalid index" << std::endl;
 	}
-	else
-		std::cout << "Invalid index" << std::endl;
 }
