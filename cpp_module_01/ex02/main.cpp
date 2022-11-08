26e440485601d79c6a210a279c3038a6a606d186
aamajane <aamajane@student.1337.ma>
2022-11-08 22:56:56 +0100

    update

--- cpp_module_01/ex02/main.cpp
+++ cpp_module_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 22:22:19 by aamajane          #+#    #+#             */
-/*   Updated: 2022/11/08 22:54:08 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/11/08 22:56:47 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,9 @@ int main(void)
 	std::string  string = "HI THIS IS BRAIN";
 	std::string* stringPTR = &string;
 	std::string& stringREF = string;
-
 	std::cout << "The memory address of the string variable: " << &string << std::endl;
 	std::cout << "The memory address held by stringPTR     : " << stringPTR << std::endl;
 	std::cout << "The memory address held by stringREF     : " << &stringREF << std::endl;
-
 	std::cout << "The value of the string variable         : " << string << std::endl;
 	std::cout << "The value pointed to by stringPTR        : " << *stringPTR << std::endl;
 	std::cout << "The value pointed to by stringREF        : " << stringREF << std::endl;
