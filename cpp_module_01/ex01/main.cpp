ffb471a0843b58faa70487fe03008ebbddc73f56
aamajane <aamajane@student.1337.ma>
2022-11-08 22:08:06 +0100

    update

--- cpp_module_01/ex01/main.cpp
+++ cpp_module_01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 23:21:35 by aamajane          #+#    #+#             */
-/*   Updated: 2022/11/08 22:00:09 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/11/08 22:06:35 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int	main(int ac, char **av)
 {
-	if (ac == 2)
+	if (ac == 3)
 	{
 		int	N = atoi(av[1]);
 		if (N)
 		{
-			Zombie*	zombie = zombieHorde(N, "Zombie");
+			Zombie*	zombie = zombieHorde(N, av[2]);
 			for (int i = 0; i < N; i++)
 				zombie[i].announce();
 			delete[] zombie;
