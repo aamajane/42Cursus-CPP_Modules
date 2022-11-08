66f41a41cb69587c9c12dc30672aa7dbc8231904
aamajane <aamajane@student.1337.ma>
2022-11-08 22:08:58 +0100

    update

--- cpp_module_01/ex01/main.cpp
+++ cpp_module_01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 23:21:35 by aamajane          #+#    #+#             */
-/*   Updated: 2022/11/08 22:06:35 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/11/08 22:08:32 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ int	main(int ac, char **av)
 		int	N = atoi(av[1]);
 		if (N)
 		{
-			Zombie*	zombie = zombieHorde(N, av[2]);
+			std::string	name = av[2];
+			Zombie*	zombie = zombieHorde(N, name);
 			for (int i = 0; i < N; i++)
 				zombie[i].announce();
 			delete[] zombie;
