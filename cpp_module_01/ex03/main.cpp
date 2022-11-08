ae60220a63ce28532a60ee25d49409c419093489
aamajane <aamajane@student.1337.ma>
2022-11-08 23:28:33 +0100

    start ex03 module 01

---
+++ cpp_module_01/ex03/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 23:26:20 by aamajane          #+#    #+#             */
+/*   Updated: 2022/11/08 23:26:48 by aamajane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// int main()
+// {
+// {
+// Weapon club = Weapon("crude spiked club");
+// HumanA bob("Bob", club);
+// bob.attack();
+// club.setType("some other type of club");
+// bob.attack();
+// }
+// {
+// Weapon club = Weapon("crude spiked club");
+// HumanB jim("Jim");
+// jim.setWeapon(club);
+// jim.attack();
+// club.setType("some other type of club");
+// jim.attack();
+// }
+// return 0;
+// }
