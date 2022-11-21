4a08bef91c6bea930c7e7b0c5ebbfa6cfcc7b655
aamajane <aamajane@student.1337.ma>
2022-11-21 14:00:20 +0100

    start ex04 module 01

---
+++ cpp_module_01/ex04/FileClass.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FileClass.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 13:12:06 by aamajane          #+#    #+#             */
+/*   Updated: 2022/11/21 13:18:14 by aamajane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILECLASS_HPP
+#define FILECLASS_HPP
+
+#include <iostream>
+
+class FileClass
+{
+	private:
+		
+
+	public:
+		
+};
+
+#endif
