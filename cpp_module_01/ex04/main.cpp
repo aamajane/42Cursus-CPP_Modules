8f55d59c786f346a85e7ea0ec2874e284c1d9941
aamajane <aamajane@student.1337.ma>
2022-11-22 16:24:01 +0100

    update

--- cpp_module_01/ex04/main.cpp
+++ cpp_module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 13:12:03 by aamajane          #+#    #+#             */
-/*   Updated: 2022/11/21 13:46:12 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/11/22 16:23:34 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ int main(int ac, char **av)
 		std::cerr << "Error: Wrong number of arguments" << std::endl;
 	else
 	{
-		
+		std::string filename = av[1];
+		std::string s1 = av[2];
+		std::string s2 = av[3];
 	}
 	return 0;
 }
