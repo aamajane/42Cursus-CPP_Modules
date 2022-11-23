597dde4fcc3f8ce366b9dbf574ddf5b344e73236
aamajane <aamajane@student.1337.ma>
2022-11-23 14:41:03 +0100

    update

--- cpp_module_01/ex04/FileClass.cpp
+++ cpp_module_01/ex04/FileClass.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 13:12:08 by aamajane          #+#    #+#             */
-/*   Updated: 2022/11/22 18:00:14 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/11/23 14:39:32 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,12 @@
 FileClass::FileClass(std::string filename, std::string s1, std::string s2)
 	: _filename(filename), _s1(s1), _s2(s2)
 {
+	std::ifstream file(filename);
+	std::ofstream newFile(filename + "replace");
+	std::string line;
+	if(!file.is_open())
+	{
+		std::cerr << "Error: File could not be opened" << std::endl;
+		return ;
+	}
 }
--- cpp_module_01/ex04/main.cpp
+++ cpp_module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 13:12:03 by aamajane          #+#    #+#             */
-/*   Updated: 2022/11/22 17:59:10 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/11/23 14:36:47 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int main(int ac, char **av)
 	if (ac != 4)
 		std::cerr << "Error: Wrong number of arguments" << std::endl;
 	else
-	{
 		FileClass fileObject(av[1], av[2], av[3]);
-	}
 	return 0;
 }
