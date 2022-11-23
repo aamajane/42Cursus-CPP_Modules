b2f51f243e8c2832dfc6a6e105600ce9006a1922
aamajane <aamajane@student.1337.ma>
2022-11-23 21:09:23 +0100

    update

--- cpp_module_01/ex04/main.cpp
+++ cpp_module_01/ex04/main.cpp
@@ -6,17 +6,39 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 13:12:03 by aamajane          #+#    #+#             */
-/*   Updated: 2022/11/23 14:36:47 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/11/23 21:08:29 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "FileClass.hpp"
+#include <iostream>
+#include <fstream>
 
 int main(int ac, char **av)
 {
 	if (ac != 4)
 		std::cerr << "Error: Wrong number of arguments" << std::endl;
 	else
-		FileClass fileObject(av[1], av[2], av[3]);
+	{
+		std::string filename = av[1];
+		std::string s1 = av[2];
+		std::string s2 = av[3];
+		std::ifstream file(filename);
+		std::ofstream newFile(filename + "replace");
+		std::string line;
+		if(!file.is_open())
+		{
+			std::cerr << "Error: File could not be opened" << std::endl;
+			return;
+		}
+		while (getline(file, line))
+		{
+			while ((int)line.find(s1) != -1)
+			{
+				line.erase(line.find(s1), s1.length());
+				line.insert(line.find(s1), s2);
+			}
+			newFile << line << std::endl;
+		}
+	}
 	return 0;
 }
