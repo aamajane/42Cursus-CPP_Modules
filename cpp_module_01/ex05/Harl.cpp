657b12e0145caec408ad794e87c8ec56b5219779
aamajane <aamajane@student.1337.ma>
2022-11-24 19:59:57 +0100

    update

--- cpp_module_01/ex05/Harl.cpp
+++ cpp_module_01/ex05/Harl.cpp
@@ -6,8 +6,32 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 13:36:39 by aamajane          #+#    #+#             */
-/*   Updated: 2022/11/24 15:41:21 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/11/24 19:54:44 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+
+void Harl::debug(void)
+{
+	std::cout << "DEBUG" << std::endl;
+}
+
+void Harl::info(void)
+{
+	std::cout << "INFO" << std::endl;
+}
+
+void Harl::warning(void)
+{
+	std::cout << "WARNING" << std::endl;
+}
+
+void Harl::error(void)
+{
+	std::cout << "ERROR" << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+}
--- cpp_module_01/ex05/Harl.hpp
+++ cpp_module_01/ex05/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 13:36:41 by aamajane          #+#    #+#             */
-/*   Updated: 2022/11/24 15:41:16 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/11/24 19:54:48 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@
 class Harl
 {
 	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
 
 	public:
-		Harl();
-		~Harl();	
+		void complain(std::string level);
 };
 
 #endif
