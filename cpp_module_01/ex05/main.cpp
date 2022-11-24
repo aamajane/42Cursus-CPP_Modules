6c044a26191d049696e8393c3e62791969517c14
aamajane <aamajane@student.1337.ma>
2022-11-24 22:42:48 +0100

    finish ex05 module 01

--- cpp_module_01/ex05/main.cpp
+++ cpp_module_01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 13:36:43 by aamajane          #+#    #+#             */
-/*   Updated: 2022/11/24 21:35:32 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/11/24 22:36:47 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 int main(void)
 {
 	Harl harl;
-	std::cout << "********** DEBUG TEST **********" << std::endl;
+	std::cout << "********** TEST 1 **********" << std::endl;
 	harl.complain("DEBUG");
-	std::cout << "********** INFO TEST **********" << std::endl;
+	std::cout << "********** TEST 2 **********" << std::endl;
 	harl.complain("INFO");
-	std::cout << "********** WARNING TEST **********" << std::endl;
+	std::cout << "********** TEST 3 **********" << std::endl;
 	harl.complain("WARNING");
-	std::cout << "********** ERROR TEST **********" << std::endl;
+	std::cout << "********** TEST 4 **********" << std::endl;
 	harl.complain("ERROR");
-	std::cout << "********** OTHERS TEST **********" << std::endl;
+	std::cout << "********** TEST 5 **********" << std::endl;
 	harl.complain("OTHERS");
 	return 0;
 }
