fa40a2103818f8c6bef1de19ea7fd3c5b1a79db8
aamajane <aamajane@student.1337.ma>
2022-11-26 17:12:42 +0100

    update

--- cpp_module_01/ex05/main.cpp
+++ cpp_module_01/ex05/main.cpp
@@ -6,24 +6,18 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 13:36:43 by aamajane          #+#    #+#             */
-/*   Updated: 2022/11/24 22:36:47 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/11/26 17:05:19 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main(void)
+int main(int ac, char **av)
 {
 	Harl harl;
-	std::cout << "********** TEST 1 **********" << std::endl;
-	harl.complain("DEBUG");
-	std::cout << "********** TEST 2 **********" << std::endl;
-	harl.complain("INFO");
-	std::cout << "********** TEST 3 **********" << std::endl;
-	harl.complain("WARNING");
-	std::cout << "********** TEST 4 **********" << std::endl;
-	harl.complain("ERROR");
-	std::cout << "********** TEST 5 **********" << std::endl;
-	harl.complain("OTHERS");
+	if (ac == 2)
+		harl.complain(av[1]);
+	else
+		std::cout << "Error: Wrong number of arguments" << std::endl;
 	return 0;
 }
