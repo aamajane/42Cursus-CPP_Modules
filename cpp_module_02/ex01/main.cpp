ae10c507ced09658b8070afdbe665752260b91e8
aamajane <aamajane@student.1337.ma>
2022-12-09 15:46:55 +0100

    update

--- cpp_module_02/ex01/main.cpp
+++ cpp_module_02/ex01/main.cpp
@@ -6,23 +6,30 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 14:33:06 by aamajane          #+#    #+#             */
-/*   Updated: 2022/12/09 15:32:59 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/12/09 15:46:49 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main(void)
+int	main(void)
 {
 	Fixed	a;
-	Fixed	b(a);
-	Fixed	c;
+	Fixed	const b(10);
+	Fixed	const c(42.42f);
+	Fixed	const d(b);
 
-	c = b;
+	a = Fixed(1234.4321f);
 
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 
 	return 0;
 }
