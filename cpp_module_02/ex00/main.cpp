e066882dd69f7afd9880d42803c26b92aef03f4c
aamajane <aamajane@student.1337.ma>
2022-12-09 15:46:09 +0100

    update

--- cpp_module_02/ex00/main.cpp
+++ cpp_module_02/ex00/main.cpp
@@ -6,13 +6,13 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 14:33:06 by aamajane          #+#    #+#             */
-/*   Updated: 2022/12/09 15:36:16 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/12/09 15:45:58 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main(void)
+int	main(void)
 {
 	Fixed	a;
 	Fixed	b(a);
