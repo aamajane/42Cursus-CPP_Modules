6268c761245f8172e5d9c9d32304a40d0c788d88
aamajane <aamajane@student.1337.ma>
2022-12-13 18:14:28 +0100

    update

--- cpp_module_02/ex03/Fixed.hpp
+++ cpp_module_02/ex03/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 14:33:04 by aamajane          #+#    #+#             */
-/*   Updated: 2022/12/13 18:02:49 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/12/13 18:13:20 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ class Fixed
 
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
+
 		float	toFloat(void) const;
 		int		toInt(void) const;
 
