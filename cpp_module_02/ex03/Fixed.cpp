02af20350cf75eab02cbdf8acf646ce301caf255
aamajane <aamajane@student.1337.ma>
2022-12-20 17:29:03 +0100

    update

--- cpp_module_02/ex03/Fixed.cpp
+++ cpp_module_02/ex03/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 14:33:01 by aamajane          #+#    #+#             */
-/*   Updated: 2022/12/20 15:42:04 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/12/20 17:28:48 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ Fixed::Fixed(int const value)
 
 Fixed::Fixed(float const value)
 {
-	_value = roundf(value * (1 << _fractionalBits));
+	_value = (int)roundf(value * (1 << _fractionalBits));
 }
 
 Fixed::~Fixed()
