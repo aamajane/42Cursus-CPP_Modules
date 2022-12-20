303e7d819eed0c2b8150a7f359e4292d0eca58e0
aamajane <aamajane@student.1337.ma>
2022-12-20 15:38:00 +0100

    update

--- cpp_module_02/ex01/Fixed.cpp
+++ cpp_module_02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 14:33:01 by aamajane          #+#    #+#             */
-/*   Updated: 2022/12/19 22:30:27 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/12/20 15:37:32 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ Fixed::Fixed(Fixed const &copy)
 Fixed::Fixed(int const value)
 {
 	std::cout << "Int constructor called" << std::endl;
-	_value = value * (1 << _fractionalBits);
+	_value = value << _fractionalBits;
 }
 
 Fixed::Fixed(float const value)
@@ -65,7 +65,7 @@ float	Fixed::toFloat(void) const
 
 int	Fixed::toInt(void) const
 {
-	return _value / (1 << _fractionalBits);
+	return _value >> _fractionalBits;
 }
 
 std::ostream	&operator<<(std::ostream &out, Fixed const &value)
