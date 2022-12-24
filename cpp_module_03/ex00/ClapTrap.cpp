09f12ae5b3d66c8aa4cd6debc0a6a8925e497adf
aamajane <aamajane@student.1337.ma>
2022-12-24 18:58:12 +0100

    update

--- cpp_module_03/ex00/ClapTrap.cpp
+++ cpp_module_03/ex00/ClapTrap.cpp
@@ -6,7 +6,29 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 17:36:24 by aamajane          #+#    #+#             */
-/*   Updated: 2022/12/24 17:36:25 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/12/24 18:51:48 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+{
+	std::cout << "ClapTrap default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string const name) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << "String ClapTrap constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(ClapTrap const &copy)
+{
+	std::cout << "ClapTrap copy constructor called" << std::endl;
+	*this = copy;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "ClapTrap destructor called" << std::endl;
+}
--- cpp_module_03/ex00/ClapTrap.hpp
+++ cpp_module_03/ex00/ClapTrap.hpp
@@ -6,7 +6,34 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 17:36:28 by aamajane          #+#    #+#             */
-/*   Updated: 2022/12/24 17:36:29 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/12/24 18:13:11 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+	private:
+		std::string	_name;
+		int			_hitPoints;
+		int			_energyPoints;
+		int			_attackDamage;
+
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		ClapTrap(ClapTrap const &copy);
+		~ClapTrap();
+
+		ClapTrap	&operator=(ClapTrap const &copy);
+
+		void	attack(std::string const &target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+};
+
+#endif
