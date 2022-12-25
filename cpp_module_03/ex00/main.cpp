862b9a314717e6163fa86c138cb7c3a133096c46
aamajane <aamajane@student.1337.ma>
2022-12-25 20:42:42 +0100

    update

--- cpp_module_03/ex00/main.cpp
+++ cpp_module_03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 17:36:30 by aamajane          #+#    #+#             */
-/*   Updated: 2022/12/25 20:40:43 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/12/25 20:42:21 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,10 @@ int	main()
 	one.attack("Enemy");
 	one.takeDamage(5);
 	one.beRepaired(3);
-
-	ClapTrap	two("Two");
-	two.attack("Enemy");
-	two.takeDamage(10);
-	two.attack("Enemy");
-	two.beRepaired(10);
-	two.takeDamage(5);
+	one.attack("Enemy");
+	one.takeDamage(10);
+	one.attack("Enemy");
+	one.beRepaired(10);
+	one.takeDamage(5);
 	return 0;
 }
