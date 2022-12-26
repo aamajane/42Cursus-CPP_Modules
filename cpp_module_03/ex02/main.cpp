d4183ea56e0d6729dae9cc6e88ee99f662f5cbd7
aamajane <aamajane@student.1337.ma>
2022-12-26 21:57:43 +0100

    update

--- cpp_module_03/ex02/main.cpp
+++ cpp_module_03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 17:36:30 by aamajane          #+#    #+#             */
-/*   Updated: 2022/12/26 18:49:45 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/12/26 21:57:07 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,8 @@ int	main()
 	one.highFivesGuys();
 	one.attack("Enemy");
 	one.takeDamage(50);
-	one.beRepaired(30);
+	one.beRepaired(50);
 	one.takeDamage(100);
-	one.highFivesGuys();
 	one.attack("Enemy");
 	one.beRepaired(100);
 	one.takeDamage(50);
