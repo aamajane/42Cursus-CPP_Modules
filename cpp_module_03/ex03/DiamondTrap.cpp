71626e8f26f6b63db99046b8e741cb0d9aa27509
aamajane <aamajane@student.1337.ma>
2022-12-27 17:22:57 +0100

    update

--- cpp_module_03/ex03/DiamondTrap.cpp
+++ cpp_module_03/ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 19:32:29 by aamajane          #+#    #+#             */
-/*   Updated: 2022/12/27 17:21:45 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/12/27 17:22:56 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,6 @@ void	DiamondTrap::attack(std::string const &target)
 
 void	DiamondTrap::whoAmI()
 {
-	
 	std::cout << "DiamondTrap name: " << this->_name << std::endl;
 	std::cout << "ClapTrap name   : " << ClapTrap::_name << std::endl;
 }
