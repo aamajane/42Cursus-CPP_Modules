da9333e6e070613bf05f2ac9236a84a7d2cabf2c
aamajane <aamajane@student.1337.ma>
2022-12-27 18:18:29 +0100

    update

--- cpp_module_03/ex03/FragTrap.cpp
+++ cpp_module_03/ex03/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 00:01:44 by aamajane          #+#    #+#             */
-/*   Updated: 2022/12/27 17:10:14 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/12/27 18:17:33 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 FragTrap::FragTrap() : ClapTrap()
 {
 	std::cout << "FragTrap default constructor called" << std::endl;
+	this->name = "Default";
 	this->_healthPoints = 100;
 	this->_energyPoints = 100;
 	this->_attackDamage = 30;
@@ -23,6 +24,7 @@ FragTrap::FragTrap() : ClapTrap()
 FragTrap::FragTrap(std::string name) : ClapTrap(name)
 {
 	std::cout << "FragTrap string constructor called" << std::endl;
+	this->name = name;
 	this->_healthPoints = 100;
 	this->_energyPoints = 100;
 	this->_attackDamage = 30;
@@ -31,6 +33,7 @@ FragTrap::FragTrap(std::string name) : ClapTrap(name)
 FragTrap::FragTrap(FragTrap const &copy) : ClapTrap(copy)
 {
 	std::cout << "FragTrap copy constructor called" << std::endl;
+	*this = copy;
 }
 
 FragTrap::~FragTrap()
