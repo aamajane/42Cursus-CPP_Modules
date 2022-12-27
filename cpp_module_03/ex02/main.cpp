13132c27cb481e2680ebe58fb50aef820847a3d9
aamajane <aamajane@student.1337.ma>
2022-12-27 17:09:36 +0100

    update

--- cpp_module_03/ex02/main.cpp
+++ cpp_module_03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 17:36:30 by aamajane          #+#    #+#             */
-/*   Updated: 2022/12/27 15:26:01 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/12/27 17:08:45 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,15 @@
 int	main()
 {
 	FragTrap	one("one");
+
+	std::cout << std::endl << one << std::endl;
+
 	one.highFivesGuys();
 	one.attack("enemy");
 	one.beRepaired(50);
 	one.takeDamage(150);
+
+	std::cout << std::endl << one << std::endl;
 
 	// FragTrap	frag("one");
 	// ClapTrap	*clap = &frag;
