96ed680c217a44db828c0e27ba2ac78c53ef3a6e
aamajane <aamajane@student.1337.ma>
2023-01-01 16:27:16 +0100

    update

--- cpp_module_04/ex03/AMateria.hpp
+++ cpp_module_04/ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 23:02:44 by aamajane          #+#    #+#             */
-/*   Updated: 2023/01/01 16:10:05 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/01/01 16:24:52 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,26 @@
 #define AMATERIA_HPP
 
 #include <iostream>
+#include "ICharacter.hpp"
 
 class AMateria
 {
-	
+	protected:
+		std::string		type;
+		unsigned int	xp;
+
+	public:
+		AMateria();
+		AMateria(std::string const &type);
+		AMateria(AMateria const &copy);
+		AMateria &operator=(AMateria const &rhs);
+		virtual ~AMateria();
+
+		std::string const	&getType() const;
+		unsigned int		getXP() const;
+
+		virtual AMateria	*clone() const = 0;
+		virtual void		use(ICharacter &target);
 };
 
 #endif
--- cpp_module_04/ex03/ICharacter.hpp
+++ cpp_module_04/ex03/ICharacter.hpp
@@ -1,13 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   AMateria.cpp                                       :+:      :+:    :+:   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/31 23:02:45 by aamajane          #+#    #+#             */
-/*   Updated: 2023/01/01 16:14:31 by aamajane         ###   ########.fr       */
+/*   Created: 2023/01/01 16:23:49 by aamajane          #+#    #+#             */
+/*   Updated: 2023/01/01 16:26:50 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ICHARACTER_HPP
+#define ICHARACTER_HPP
+
+#include <iostream>
 #include "AMateria.hpp"
+
+class ICharacter
+{
+	public:
+		virtual ~ICharacter() {}
+
+		virtual std::string const	&getName() const = 0;
+
+		virtual void	equip(AMateria *m) = 0;
+		virtual void	unequip(int idx) = 0;
+		virtual void	use(int idx, ICharacter &target) = 0;
+};
+
+#endif
