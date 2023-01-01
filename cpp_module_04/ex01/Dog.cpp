0d9d5ac233d09e8baf08f23c4e30180af91d2258
aamajane <aamajane@student.1337.ma>
2023-01-01 15:55:21 +0100

    update

--- cpp_module_04/ex01/Dog.cpp
+++ cpp_module_04/ex01/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 23:01:45 by aamajane          #+#    #+#             */
-/*   Updated: 2022/12/31 20:58:41 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/01/01 15:55:12 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ Dog	&Dog::operator=(Dog const &copy)
 	if (this != &copy)
 	{
 		this->type = copy.type;
+		delete this->brain;
 		this->brain = new Brain(*copy.brain);
 	}
 	return *this;
