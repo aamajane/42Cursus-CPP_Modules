89effc2536dfb17070348b0dce22b1f4f069e1ce
aamajane <aamajane@student.1337.ma>
2023-01-02 15:48:25 +0100

    update

--- cpp_module_04/ex03/Character.cpp
+++ cpp_module_04/ex03/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 17:47:59 by aamajane          #+#    #+#             */
-/*   Updated: 2023/01/01 17:59:04 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/01/02 15:46:07 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ Character::Character(Character const &copy)
 Character &Character::operator=(Character const &rhs)
 {
 	this->name = rhs.name;
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < 4 && this->slot[i]; i++)
 		delete this->slot[i];
 	for (int i = 0; i < 4; i++)
 		this->slot[i] = rhs.slot[i]->clone();
@@ -54,7 +54,7 @@ void	Character::equip(AMateria *m)
 {
 	for (int i = 0; i < 4; i++)
 	{
-		if (this->slot[i] == NULL)
+		if (!this->slot[i])
 		{
 			this->slot[i] = m;
 			return ;
@@ -65,13 +65,15 @@ void	Character::equip(AMateria *m)
 void	Character::unequip(int idx)
 {
 	if (idx < 0 || idx > 3)
-		return ;
-	this->slot[idx] = NULL;
+		std::cout << "Invalid index" << std::endl;
+	else
+		this->slot[idx] = NULL;
 }
 
 void	Character::use(int idx, ICharacter &target)
 {
 	if (idx < 0 || idx > 3)
-		return ;
-	this->slot[idx]->use(target);
+		std::cout << "Invalid index" << std::endl;
+	else
+		this->slot[idx]->use(target);
 }
