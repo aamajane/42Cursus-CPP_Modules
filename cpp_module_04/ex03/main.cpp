9ffd26f30a43047175ba12ba8d6622501f93f762
aamajane <aamajane@student.1337.ma>
2023-01-03 22:38:09 +0100

    update

--- cpp_module_04/ex03/main.cpp
+++ cpp_module_04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 23:01:51 by aamajane          #+#    #+#             */
-/*   Updated: 2023/01/01 18:22:23 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/01/03 22:37:33 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,13 @@
 int main()
 {
 	IMateriaSource	*src = new MateriaSource();
+
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
 
 	ICharacter	*me = new Character("me");
-
 	AMateria	*tmp;
+
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
