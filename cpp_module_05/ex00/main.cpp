fd13fa6d4c6aef5bba5b7e0296bdb3f5f5fc1fc1
aamajane <aamajane@student.1337.ma>
2023-01-06 16:42:08 +0100

    update

--- cpp_module_05/ex00/main.cpp
+++ cpp_module_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 16:13:55 by aamajane          #+#    #+#             */
-/*   Updated: 2023/01/06 16:38:55 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/01/06 16:41:43 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	main()
 	{
 		Bureaucrat	bureaucrat("One", 1);
 		std::cout << bureaucrat;
+		bureaucrat.incrementGrade();
+		std::cout << bureaucrat;
 	}
 	catch (std::exception &e)
 	{
@@ -29,6 +31,8 @@ int	main()
 	try
 	{
 		Bureaucrat	bureaucrat("Tow", 150);
+		std::cout << bureaucrat;
+		bureaucrat.decrementGrade();
 		std::cout << bureaucrat;
 	}
 	catch (std::exception &e)
@@ -51,30 +55,6 @@ int	main()
 	try
 	{
 		Bureaucrat	bureaucrat("Four", 151);
-		std::cout << bureaucrat;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-
-	std::cout << "=============== TEST5 ===============" << std::endl;
-	try
-	{
-		Bureaucrat	bureaucrat("Five", 1);
-		bureaucrat.incrementGrade();
-		std::cout << bureaucrat;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-
-	std::cout << "=============== TEST6 ===============" << std::endl;
-	try
-	{
-		Bureaucrat	bureaucrat("Six", 150);
-		bureaucrat.decrementGrade();
 		std::cout << bureaucrat;
 	}
 	catch (std::exception &e)
