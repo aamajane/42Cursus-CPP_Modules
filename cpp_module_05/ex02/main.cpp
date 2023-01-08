e7a27d9ddc0d1ea78162e8936f90cabbdfb888fe
aamajane <aamajane@student.1337.ma>
2023-01-08 01:19:38 +0100

    update

--- cpp_module_05/ex02/main.cpp
+++ cpp_module_05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 16:13:55 by aamajane          #+#    #+#             */
-/*   Updated: 2023/01/08 01:01:31 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/01/08 01:19:25 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,78 +30,37 @@ int	main(int ac, char **av)
 		return 0;
 	}
 
-	int			test = atoi(av[1]);
-	std::string	target = av[2];
-	std::string	name = av[3];
-	int			grade = atoi(av[4]);
-
-	if (test == 1)
+	try
 	{
-		try
-		{
-			Form		*form = new ShrubberyCreationForm(target);
-			Bureaucrat	*bureaucrat = new Bureaucrat(name, grade);
+		int			test = atoi(av[1]);
+		std::string	target = av[2];
+		std::string	name = av[3];
+		int			grade = atoi(av[4]);
 
-			std::cout << *bureaucrat;
-			std::cout << *form;
-			bureaucrat->executeForm(*form);
-			bureaucrat->signForm(*form);
-			std::cout << *form;
-			bureaucrat->executeForm(*form);
+		Form		*form = NULL;
+		Bureaucrat	bureaucrat(name, grade);
+	
+		if (test == 1)
+			form = new ShrubberyCreationForm(target);
 
-			delete bureaucrat;
-			delete form;
-		}
-		catch (std::exception &e)
-		{
-			std::cout << e.what() << std::endl;
-		}
+		else if (test == 2)
+			form = new RobotomyRequestForm(target);
+
+		else if (test == 3)
+			form = new PresidentialPardonForm(target);
+
+		std::cout << bureaucrat;
+		std::cout << *form;
+		bureaucrat.executeForm(*form);
+		bureaucrat.signForm(*form);
+		std::cout << *form;
+		bureaucrat.executeForm(*form);
+
+		delete form;
 	}
-
-	else if (test == 2)
+	catch (std::exception &e)
 	{
-		try
-		{
-			Form		*form = new RobotomyRequestForm(target);
-			Bureaucrat	*bureaucrat = new Bureaucrat(name, grade);
-
-			std::cout << *bureaucrat;
-			std::cout << *form;
-			bureaucrat->executeForm(*form);
-			bureaucrat->signForm(*form);
-			std::cout << *form;
-			bureaucrat->executeForm(*form);
-
-			delete bureaucrat;
-			delete form;
-		}
-		catch (std::exception &e)
-		{
-			std::cout << e.what() << std::endl;
-		}
-	}
-
-	else if (test == 3)
-	{
-		try
-		{
-			Form		*form = new PresidentialPardonForm(target);
-			Bureaucrat	*bureaucrat = new Bureaucrat(name, grade);
-
-			std::cout << *bureaucrat;
-			std::cout << *form;
-			bureaucrat->executeForm(*form);
-			bureaucrat->signForm(*form);
-			std::cout << *form;
-			bureaucrat->executeForm(*form);
-
-			delete bureaucrat;
-			delete form;
-		}
-		catch (std::exception &e)
-		{
-			std::cout << e.what() << std::endl;
-		}
+		std::cout << e.what() << std::endl;
 	}
 
 	return 0;
