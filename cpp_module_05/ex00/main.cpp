d22911696f58796638697a52f28cdb448ff643b1
aamajane <aamajane@student.1337.ma>
2023-01-08 01:07:01 +0100

    update

--- cpp_module_05/ex00/main.cpp
+++ cpp_module_05/ex00/main.cpp
@@ -6,59 +6,35 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 16:13:55 by aamajane          #+#    #+#             */
-/*   Updated: 2023/01/06 21:29:31 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/01/08 01:05:35 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-int	main()
+int	main(int ac, char **av)
 {
-	std::cout << "=============== TEST1 ===============" << std::endl;
+	if (ac != 3)
+	{
+		std::cout << "Usage: "
+		<< "./program "
+		<< "[Bureaucrat name] "
+		<< "[Bureaucrat grade]" << std::endl;
+
+		return 0;
+	}
+
 	try
 	{
-		Bureaucrat	bureaucrat("OMAR", 2);
+		std::string	name = av[1];
+		int			grade = atoi(av[2]);
+
+		Bureaucrat	bureaucrat(name, grade);
+
 		std::cout << bureaucrat;
 		bureaucrat.incrementGrade();
 		std::cout << bureaucrat;
-		bureaucrat.incrementGrade();
-		std::cout << bureaucrat;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-
-	std::cout << "=============== TEST2 ===============" << std::endl;
-	try
-	{
-		Bureaucrat	bureaucrat("AYOUB", 149);
-		std::cout << bureaucrat;
 		bureaucrat.decrementGrade();
-		std::cout << bureaucrat;
-		bureaucrat.decrementGrade();
-		std::cout << bureaucrat;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-
-	std::cout << "=============== TEST3 ===============" << std::endl;
-	try
-	{
-		Bureaucrat	bureaucrat("ANASS", 0);
-		std::cout << bureaucrat;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-
-	std::cout << "=============== TEST4 ===============" << std::endl;
-	try
-	{
-		Bureaucrat	bureaucrat("MOHAMMED", 151);
 		std::cout << bureaucrat;
 	}
 	catch (std::exception &e)
