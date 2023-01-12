919b908f40a274558e23ee7cf68ccc3664b1468f
aamajane <aamajane@student.1337.ma>
2023-01-12 01:44:40 +0100

    update

---
+++ cpp_module_06/ex02/main.cpp
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 01:30:53 by aamajane          #+#    #+#             */
+/*   Updated: 2023/01/12 01:44:13 by aamajane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class Base
+{
+	public:
+		virtual ~Base() {}
+};
+
+class A : public Base {};
+
+class B : public Base {};
+
+class C : public Base {};
+
+Base	*generate()
+{
+	int i = time(NULL) % 3;
+
+	if (i == 0)
+		return (new A);
+	else if (i == 1)
+		return (new B);
+	else
+		return (new C);
+}
+
+void	identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Base" << std::endl;
+}
+
+void	identify(Base& p)
+{
+	try
+	{
+		A &a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "A" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		try
+		{
+			B &b = dynamic_cast<B&>(p);
+			(void)b;
+			std::cout << "B" << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			try
+			{
+				C &c = dynamic_cast<C&>(p);
+				(void)c;
+				std::cout << "C" << std::endl;
+			}
+			catch(const std::exception& e)
+			{
+				std::cout << "Base" << std::endl;
+			}
+		}
+	}
+}
+
+int main()
+{
+	Base	*p = generate();
+
+	identify(p);
+	identify(*p);
+
+	delete p;
+
+	return 0;
+}
