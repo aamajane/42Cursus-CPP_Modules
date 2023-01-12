d55dfe7129c7decb6feb163ca293d0e341a73e53
aamajane <aamajane@student.1337.ma>
2023-01-12 15:17:10 +0100

    update

--- cpp_module_06/ex00/main.cpp
+++ cpp_module_06/ex00/main.cpp
@@ -6,11 +6,12 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 17:05:45 by aamajane          #+#    #+#             */
-/*   Updated: 2023/01/12 15:13:14 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/01/12 15:16:12 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cfloat>
 
 #define CHAR		0
 #define INT			1
@@ -65,19 +66,19 @@ void	convert(int type, std::string s)
 	else if (type == DOUBLE)
 		n = std::stod(s);
 
-	if (type > CHAR && (n < std::numeric_limits<char>::min() || n > std::numeric_limits<char>::max()))
+	if (type > CHAR && (n < CHAR_MIN || n > CHAR_MAX))
 		std::cout << "char   : impossible" << std::endl;
 	else if (std::isprint(static_cast<int>(n)))
 		std::cout << "char   : '" << static_cast<char>(n) << "'" << std::endl;
 	else
 		std::cout << "char   : Non displayable" << std::endl;
 
-	if (type > INT && (n < std::numeric_limits<int>::min() || n > std::numeric_limits<int>::max()))
+	if (type > INT && (n < INT_MIN || n > INT_MAX))
 		std::cout << "int    : impossible" << std::endl;
 	else
 		std::cout << "int    : " << static_cast<int>(n) << std::endl;
 
-	if (type > FLOAT && (n < std::numeric_limits<float>::min() || n > std::numeric_limits<float>::max()))
+	if (type > FLOAT && (n < FLT_MIN || n > FLT_MAX))
 		std::cout << "float  : impossible" << std::endl;
 	else
 		std::cout << "float  : " << static_cast<float>(n) << "f" << std::endl;
