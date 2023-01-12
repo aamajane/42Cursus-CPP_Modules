9243ea8de716ee7b89d315d31d5130ed9f1e6de0
aamajane <aamajane@student.1337.ma>
2023-01-12 01:22:18 +0100

    update

---
+++ cpp_module_06/ex01/main.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 00:57:12 by aamajane          #+#    #+#             */
+/*   Updated: 2023/01/12 01:21:09 by aamajane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+struct Data
+{
+	std::string s;
+	int 		n;
+};
+
+uintptr_t	serialize(Data *ptr)
+{
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data	*deserialize(uintptr_t raw)
+{
+	return reinterpret_cast<Data *>(raw);
+}
+
+int	main()
+{
+	Data	*data = new Data;
+
+	data->s = "Hello World!";
+	data->n = 42;
+
+	uintptr_t	ser = serialize(data);
+	Data		*des = deserialize(ser);
+
+	if (data == des)
+	{
+		std::cout << "Success!" << std::endl;
+
+		std::cout << "data adderss: " << data << std::endl;
+		std::cout << "des address : " << des << std::endl;
+
+		std::cout << "data string : " << data->s << std::endl;
+		std::cout << "des string  : " << des->s << std::endl;
+		std::cout << "data int    : " << data->n << std::endl;
+		std::cout << "des int     : " << des->n << std::endl;
+		
+	}
+	else
+		std::cout << "Failure!" << std::endl;
+	
+	return 0;
+}
