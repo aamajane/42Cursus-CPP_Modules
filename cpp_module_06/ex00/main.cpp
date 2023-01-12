16c89312c4bcd9f01f224885052ee833fcf07fde
aamajane <aamajane@student.1337.ma>
2023-01-12 15:14:58 +0100

    update

--- cpp_module_06/ex00/main.cpp
+++ cpp_module_06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 17:05:45 by aamajane          #+#    #+#             */
-/*   Updated: 2023/01/12 02:12:12 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/01/12 15:13:14 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,68 +54,35 @@ int	detectType(std::string s)
 	return IMPOSSIBLE;
 }
 
-void	convertFromChar(std::string s)
+void	convert(int type, std::string s)
 {
-	char	c = s.at(1);
+	double	n = s.at(1);
 
-	std::cout << "char   : '" << c << "'" << std::endl;
-	std::cout << "int    : " << static_cast<int>(c) << std::endl;
-	std::cout << "float  : " << static_cast<float>(c) << ".0f" << std::endl;
-	std::cout << "double : " << static_cast<double>(c) << ".0" << std::endl;
-}
+	if (type == INT)
+		n = std::stoi(s);
+	else if (type == FLOAT)
+		n = std::stof(s);
+	else if (type == DOUBLE)
+		n = std::stod(s);
 
-void	convertFromInt(std::string s)
-{
-	int	n = std::stoi(s);
-
-	if (n < std::numeric_limits<char>::min() || n > std::numeric_limits<char>::max())
-		std::cout << "char   : impossible" << std::endl;
-	else if (std::isprint(n))
-		std::cout << "char   : '" << static_cast<char>(n) << "'" << std::endl;
-	else
-		std::cout << "char   : Non displayable" << std::endl;
-	std::cout << "int    : " << static_cast<int>(n) << std::endl;
-	std::cout << "float  : " << static_cast<float>(n) << ".0f" << std::endl;
-	std::cout << "double : " << static_cast<double>(n) << ".0" << std::endl;
-}
-
-void	convertFromFloat(std::string s)
-{
-	float	n = std::stof(s);
-
-	if (n < std::numeric_limits<char>::min() || n > std::numeric_limits<char>::max())
+	if (type > CHAR && (n < std::numeric_limits<char>::min() || n > std::numeric_limits<char>::max()))
 		std::cout << "char   : impossible" << std::endl;
 	else if (std::isprint(static_cast<int>(n)))
 		std::cout << "char   : '" << static_cast<char>(n) << "'" << std::endl;
 	else
 		std::cout << "char   : Non displayable" << std::endl;
-	if (n < std::numeric_limits<int>::min() || n > std::numeric_limits<int>::max())
+
+	if (type > INT && (n < std::numeric_limits<int>::min() || n > std::numeric_limits<int>::max()))
 		std::cout << "int    : impossible" << std::endl;
 	else
 		std::cout << "int    : " << static_cast<int>(n) << std::endl;
-	std::cout << "float  : " << static_cast<float>(n) << "f" << std::endl;
-	std::cout << "double : " << static_cast<double>(n) << std::endl;
-}
 
-void	convertFromDouble(std::string s)
-{
-	double	n = std::stod(s);
-
-	if (n < std::numeric_limits<char>::min() || n > std::numeric_limits<char>::max())
-		std::cout << "char   : impossible" << std::endl;
-	else if (std::isprint(static_cast<int>(n)))
-		std::cout << "char   : '" << static_cast<char>(n) << "'" << std::endl;
-	else
-		std::cout << "char   : Non displayable" << std::endl;
-	if (n < std::numeric_limits<int>::min() || n > std::numeric_limits<int>::max())
-		std::cout << "int    : impossible" << std::endl;
-	else
-		std::cout << "int    : " << static_cast<int>(n) << std::endl;
-	if (n < std::numeric_limits<float>::min() || n > std::numeric_limits<float>::max())
+	if (type > FLOAT && (n < std::numeric_limits<float>::min() || n > std::numeric_limits<float>::max()))
 		std::cout << "float  : impossible" << std::endl;
 	else
 		std::cout << "float  : " << static_cast<float>(n) << "f" << std::endl;
-	std::cout << "double : " << n << std::endl;
+
+	std::cout << "double : " << static_cast<double>(n) << "" << std::endl;
 }
 
 int	main(int ac, char **av)
@@ -131,13 +98,9 @@ int	main(int ac, char **av)
 			std::cout << "impossible for all types" << std::endl;
 		else
 		{
-			void	(*convert[4])(std::string) = {convertFromChar, 
-												  convertFromInt, 
-												  convertFromFloat, 
-												  convertFromDouble};
 			try
 			{
-				convert[type](s);
+				convert(type, s);
 			}
 			catch (std::exception &e)
 			{
