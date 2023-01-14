2fbc9db01259b1a70768d7caf0ea5ad0c1d8e22b
aamajane <aamajane@student.1337.ma>
2023-01-14 18:27:31 +0100

    update

--- cpp_module_08/ex00/easyfind.hpp
+++ cpp_module_08/ex00/easyfind.hpp
@@ -6,19 +6,20 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 22:46:55 by aamajane          #+#    #+#             */
-/*   Updated: 2023/01/14 18:15:01 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/01/14 18:27:23 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <algorithm>
 
 template <typename T>
-bool	easyfind(T &container, int n)
+bool	easyFind(T &container, int n)
 {
-	typename T::iterator	it;
+	typename T::iterator	begin = container.begin();
+	typename T::iterator	end = container.end();
+	typename T::iterator	it = std::find(begin, end, n);
 
-	it = std::find(container.begin(), container.end(), n);
-	if (it == container.end())
+	if (it == end)
 		return (false);
 	return (true);
 }
