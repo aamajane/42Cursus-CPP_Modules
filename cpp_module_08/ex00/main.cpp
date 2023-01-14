86d590d1b250c36e31809f135344098292867f5c
aamajane <aamajane@student.1337.ma>
2023-01-14 19:46:11 +0100

    update

--- cpp_module_08/ex00/main.cpp
+++ cpp_module_08/ex00/main.cpp
@@ -6,13 +6,31 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 22:46:52 by aamajane          #+#    #+#             */
-/*   Updated: 2023/01/14 18:23:14 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/01/14 19:45:42 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <vector>
+#include <iostream>
 
 int	main()
 {
+	std::vector<int>	v;
+
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+
+	try
+	{
+		std::cout << easyFind(v, 3) << std::endl;
+		std::cout << easyFind(v, 8) << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 	return 0;
 }
