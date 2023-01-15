e8a7301e53cb44c68af47c746c9f71f85998b7cd
aamajane <aamajane@student.1337.ma>
2023-01-15 20:31:54 +0100

    update

--- cpp_module_08/ex02/MutantStack.hpp
+++ cpp_module_08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 20:13:50 by aamajane          #+#    #+#             */
-/*   Updated: 2023/01/15 20:29:57 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/01/15 20:31:43 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,17 +36,17 @@ class MutantStack : public std::stack<T>
 		typedef typename std::stack<T>::container_type::reverse_iterator		reverse_iterator;
 		typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
 
-		iterator				begin() { return this->c.begin(); }
-		iterator				end() { return this->c.end(); }
+		iterator				begin()			{ return this->c.begin(); }
+		iterator				end()			{ return this->c.end(); }
 
-		const_iterator			begin() const { return this->c.begin(); }
-		const_iterator			end() const { return this->c.end(); }
+		const_iterator			begin() const	{ return this->c.begin(); }
+		const_iterator			end() const		{ return this->c.end(); }
 
-		reverse_iterator		rbegin() { return this->c.rbegin(); }
-		reverse_iterator		rend() { return this->c.rend(); }
+		reverse_iterator		rbegin()		{ return this->c.rbegin(); }
+		reverse_iterator		rend()			{ return this->c.rend(); }
 
-		const_reverse_iterator	rbegin() const { return this->c.rbegin(); }
-		const_reverse_iterator	rend() const { return this->c.rend(); }
+		const_reverse_iterator	rbegin() const	{ return this->c.rbegin(); }
+		const_reverse_iterator	rend() const	{ return this->c.rend(); }
 };
 
 #endif
