4cb6e90e24b0441090d04ed315110dcfa1a8652d
aamajane <aamajane@student.1337.ma>
2023-01-15 20:38:49 +0100

    update

--- cpp_module_08/ex02/main.cpp
+++ cpp_module_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 20:13:51 by aamajane          #+#    #+#             */
-/*   Updated: 2023/01/15 20:27:07 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/01/15 20:38:25 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,37 @@ int	main()
 	}
 
 	std::stack<int>	s(mstack);
+
+
+	// std::list<int>	mlist;
+
+	// mlist.push_back(5);
+	// mlist.push_back(17);
+
+	// std::cout << mlist.back() << std::endl;
+
+	// mlist.pop_back();
+
+	// std::cout << mlist.size() << std::endl;
+
+	// mlist.push_back(3);
+	// mlist.push_back(5);
+	// mlist.push_back(737);
+	// mlist.push_back(0);
+
+	// std::list<int>::iterator	it = mlist.begin();
+	// std::list<int>::iterator	ite = mlist.end();
+
+	// ++it;
+	// --it;
+
+	// while (it != ite)
+	// {
+	// 	std::cout << *it << std::endl;
+	// 	++it;
+	// }
+
+	// std::list<int>	s(mlist);
 
 	return 0;
 }
