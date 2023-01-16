dda4f921a36d0bef3198d32e66f713d00fec8a8f
aamajane <aamajane@student.1337.ma>
2023-01-16 21:40:57 +0100

    update

--- cpp_module_08/ex00/main.cpp
+++ cpp_module_08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 22:46:52 by aamajane          #+#    #+#             */
-/*   Updated: 2023/01/14 20:34:41 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/01/16 21:40:31 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	main()
 	try
 	{
 		std::cout << easyFind(v, 2) << std::endl;
-		std::cout << easyFind(v, 8) << std::endl;
+		std::cout << easyFind< std::vector<int> >(v, 8) << std::endl;
 	}
 	catch (std::exception &e)
 	{
@@ -42,7 +42,7 @@ int	main()
 	try
 	{
 		std::cout << easyFind(l, 6) << std::endl;
-		std::cout << easyFind(l, 0) << std::endl;
+		std::cout << easyFind< std::list<int> >(l, 0) << std::endl;
 	}
 	catch (std::exception &e)
 	{
