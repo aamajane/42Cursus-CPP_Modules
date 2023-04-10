54290dafe91aa8a546d941595921f84da74ec438
aamajane <aamajane@student.1337.ma>
2023-04-10 02:04:49 +0000

    update

--- cpp_module_09/ex02/PmergeMe.cpp
+++ cpp_module_09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 12:59:57 by aamajane          #+#    #+#             */
-/*   Updated: 2023/04/10 01:26:39 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/04/10 02:04:10 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,6 @@ void	PmergeMe::isSequenceValid(std::string const &str)
 		if (!isdigit(str[i]) && str[i] != ' ')
 			throw std::runtime_error("Error: Invalid sequence");
 
-	if (std::count(str.begin(), str.end(), ' ') == str.length())
-		throw std::runtime_error("Error: Invalid sequence");
-
 	for (size_t i = 0; i < str.length(); i++)
 	{
 		while (str[i] == ' ')
@@ -47,15 +44,14 @@ void	PmergeMe::isSequenceValid(std::string const &str)
 			std::string	tmp = "";
 			while (isdigit(str[i]))
 				tmp += str[i++];
-			if (tmp.length() > 10)
-				throw std::runtime_error("Error: Invalid sequence");
 
 			int	nbr = std::stoi(tmp);
-			if (nbr < 0)
-				throw std::runtime_error("Error: Invalid sequence");
 
 			this->_vec.push_back(nbr);
 			this->_lst.push_back(nbr);
 		}
 	}
+
+	if (this->_vec.empty())
+		throw std::runtime_error("Error: Invalid sequence");
 }
