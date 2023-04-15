69d74ce2858b3b88eb1efc971fbecbf2255cda37
aamajane <aamajane@student.1337.ma>
2023-04-15 03:22:13 +0000

    update

--- cpp_module_09/ex02/PmergeMe.cpp
+++ cpp_module_09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 12:59:57 by aamajane          #+#    #+#             */
-/*   Updated: 2023/04/14 00:55:59 by aamajane         ###   ########.fr       */
+/*   Updated: 2023/04/15 03:21:50 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ PmergeMe::PmergeMe(PmergeMe const &copy)
 
 PmergeMe &PmergeMe::operator=(PmergeMe const &rhs)
 {
-	this->_vec = rhs._vec;
-	this->_lst = rhs._lst;
+	_vec = rhs._vec;
+	_lst = rhs._lst;
 	return *this;
 }
 
@@ -43,11 +43,11 @@ void	PmergeMe::isSequenceValid(size_t ac, char **av)
 
 		int	nbr = std::stoi(str);
 
-		this->_vec.push_back(nbr);
-		this->_lst.push_back(nbr);
+		_vec.push_back(nbr);
+		_lst.push_back(nbr);
 	}
 
-	if (std::is_sorted(this->_vec.begin(), this->_vec.end()))
+	if (std::is_sorted(_vec.begin(), _vec.end()))
 		throw std::runtime_error("Error: Sequence already sorted");
 }
 
@@ -57,12 +57,12 @@ void	PmergeMe::sortSequence()
 	printSequence();
 
 	clock_t	startVec = clock();
-	this->_vec = mergeInsertionSort(this->_vec);
+	_vec = mergeInsertionSort(_vec);
 	clock_t	endVec = clock();
 	clock_t elapsedTimeVec = endVec - startVec;
 
 	clock_t startLst = clock();
-	this->_lst = mergeInsertionSort(this->_lst);
+	_lst = mergeInsertionSort(_lst);
 	clock_t endLst = clock();
 	clock_t elapsedTimeLst = endLst - startLst;
 
@@ -75,10 +75,10 @@ void	PmergeMe::sortSequence()
 
 void	PmergeMe::printSequence()
 {
-	for (size_t i = 0; i < this->_vec.size(); i++)
+	for (size_t i = 0; i < _vec.size(); i++)
 	{
-		std::cout << this->_vec[i];
-		if (i != this->_vec.size() - 1)
+		std::cout << _vec[i];
+		if (i != _vec.size() - 1)
 			std::cout << " ";
 	}
 	std::cout << std::endl;
@@ -88,8 +88,8 @@ void	PmergeMe::printTime(clock_t elapsedTime, std::string type)
 {
 	std::cout << "Time to process a range of ";
 	if (type == "vector")
-		std::cout << this->_vec.size() << " elements with std::vector : ";
+		std::cout << _vec.size() << " elements with std::vector : ";
 	else
-		std::cout << this->_lst.size() << " elements with std::list   : ";
+		std::cout << _lst.size() << " elements with std::list   : ";
 	std::cout << elapsedTime  << " us" << std::endl;
 }
